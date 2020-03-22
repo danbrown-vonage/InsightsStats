77ddb611e1bc4e45c28cc78d8cf4cef1bd22a9b7
danbrown-vonage <62491693+danbrown-vonage@users.noreply.github.com>
2020-03-22 10:34:19 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -11,6 +11,7 @@ self.addEventListener('install', function(e) {
   console.log('[Service Worker] Install');
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
+      console.log('return')
       return cache.addAll(filesToCache);
     })
   );
@@ -19,9 +20,9 @@ self.addEventListener('install', function(e) {
 /* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
   console.log('[Service Worker] Fetched resource '+e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+  // e.respondWith(
+  //   caches.match(e.request).then(function(response) {
+  //     return response || fetch(e.request);
+  //   })
+  // );
 });
